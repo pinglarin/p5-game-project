bd20ef2df2273fe9c8ae18fb7dd4df6ed67b884a
Atp-Gaster <nawat.ngr@student.mahidol.ac.th>
2022-03-29 20:31:56 +0700

    - Try to upgrade dash system

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,42 +1,67 @@
 var speed;
 const Move = true;
 var isDash = false;
+var DashDir = 0;
 let x, y;
+var song;
+
+let player;
+// function preload()
+// {
+//   song = loadSound('Cytus R - VitMaster - Sakuzyo.m4a');
+// }
 
 
 function setup() {
   createCanvas(600, 600);
-  noStroke();//borderless
+  //song = loadSound("Cytus R - VitMaster - Sakuzyo.m4a");
+  //song.play();
+  noStroke();//borderless  
   x = width / 2;  //player start position
   y = height / 2; //player start position
   speed = createVector(10,10);
   frameRate(60);
+
+
 }
 
 function draw() {
   background(16, 55, 84); 
-  fill(51, 171, 177);
-  ellipse(x, y, 50, 50); // x pos, y pos , size x , size y
-  
-
-  if(Move)
+  if(!isDash)
   {
-    if (keyIsDown(87)) //w
-    {
-      y -= speed.y;
-    }
-    else if (keyIsDown(65))//a
-    {
-      x -= speed.x;
-    } else if (keyIsDown(83))//s
-    {
-      y += speed.y;
-    } else if (keyIsDown(68))//d
-    {
-      x += speed.x;
-    }    
+    fill(51, 171, 177);
+    ellipse(x, y, 50, 50); // player size x pos, y pos , size x , size y
   }
-
+  else if(isDash && DashDir == 1)
+  {
+    fill(255, 0, 0);
+    ellipse(x, y, 25, 50); // player size x pos, y pos , size x , size y     
+  }  
+  else if(isDash && DashDir == 2)
+  {
+    fill(255, 0, 0);    
+     ellipse(x, y, 50, 25); // player size x pos, y pos , size x , size y
+  }    
+ 
+  //Control System
+  if (keyIsDown(87)) //w
+  {
+    y -= speed.y;
+  }
+  else if (keyIsDown(65))//a
+  {
+    x -= speed.x;
+  } 
+  else if (keyIsDown(83))//s
+  {
+    y += speed.y;
+  }   
+  else if (keyIsDown(68))//d
+  {
+    x += speed.x;
+  }    
+ 
+  //Wall on mapsize
   if( x > width - 25)
   {
     x = width - 25;
@@ -56,18 +81,34 @@ function draw() {
 }
 
 function keyPressed()
-{  
-  if(keyIsDown(32) && isDash == false)
-  {
+{   
+  if(keyIsDown(32) && (keyIsDown(87) || keyIsDown(83)) && isDash == false)
+  {  
+   
     speed.set(20,20);
     isDash = true;
-    print("Is Dash")
-  }  
-  else if (!keyIsDown(32) && isDash == true)
+    print("Is Dash |");
+    print(DashDir);
+   
+      DashDir = 1; //Horizon
+    
+ 
+  } 
+  if(keyIsDown(32) && (keyIsDown(65) || keyIsDown(68)) && isDash == false)
+  {    
+    speed.set(20,20);
+    isDash = true;
+    print("Is Dash - ");
+    print(DashDir);
+    
+      DashDir = 2; //Vertical
+    
+  } 
+  if (!keyIsDown(32) && isDash == true)
   {
     speed.set(10,10);
-    isDash = false;
-    
+    isDash = false; 
+    DashDir = 0;  
   }
  
 }
